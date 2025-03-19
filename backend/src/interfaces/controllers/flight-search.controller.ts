fc05402b13950d7e917728d058023aa396f2dee9
vrizzo79 <vittorio.rizzo@guccidigital.com>
2025-03-19 09:45:03 +0100

    frontend first tentative

--- backend/src/interfaces/controllers/flight-search.controller.ts
+++ backend/src/interfaces/controllers/flight-search.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
-import { FindCompatibleRoutesUseCase } from '../../application/use-cases/find-compatible-routes.use-case';
-import { MongoTravelPreferenceRepository } from '../../infrastructure/repositories/mongodb/mongo-travel-preference.repository';
-import { MongoRouteRepository } from '../../infrastructure/repositories/mongodb/mongo-route.repository';
-import { MongoFlightRepository } from '../../infrastructure/repositories/mongodb/mongo-flight.repository';
-import { SearchFlightsUseCase } from '../../application/use-cases/search-flights.use-case';
-import { KiwiClient } from '../../infrastructure/clients/kiwi/kiwi.client';
+import { FindCompatibleRoutesUseCase } from 'application/use-cases/find-compatible-routes.use-case';
+import { MongoTravelPreferenceRepository } from 'infrastructure/repositories/mongodb/mongo-travel-preference.repository';
+import { MongoRouteRepository } from 'infrastructure/repositories/mongodb/mongo-route.repository';
+import { MongoFlightRepository } from 'infrastructure/repositories/mongodb/mongo-flight.repository';
+import { SearchFlightsUseCase } from 'application/use-cases/search-flights.use-case';
+import { KiwiClient } from 'infrastructure/clients/kiwi/kiwi.client';
 
 export class FlightSearchController {
   async triggerSearch(req: Request, res: Response) {
@@ -12,10 +12,8 @@ export class FlightSearchController {
       // Initialize repositories
       const travelPreferenceRepository = new MongoTravelPreferenceRepository();
       const routeRepository = new MongoRouteRepository();
-      const flightRepository = new MongoFlightRepository();
       
       // Initialize use cases
-      const searchFlightsUseCase = new SearchFlightsUseCase(flightRepository, new KiwiClient('eXvQj9tCWpz81UgB3blKh5POjZQeTjpg'));
       const findCompatibleRoutesUseCase = new FindCompatibleRoutesUseCase(
         travelPreferenceRepository,
         routeRepository,
